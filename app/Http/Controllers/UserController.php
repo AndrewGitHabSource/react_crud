7b1cc1880487365aead624ad2386524357174ec3
Andrey Nozdrenko <developer@devit.group>
2022-09-08 18:10:41 +0300

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,26 +6,27 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\UserRequest;
+use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller {
-    public function index() {
+    public function index(): JsonResponse {
         return response()->json(User::all());
     }
 
-    public function save(UserRequest $request) {
+    public function save(UserRequest $request): void {
         $user = $request->except($request->password);
         $user['password'] = Hash::make($request->password);
         User::create($user);
     }
 
-    public function edit(UserRequest $request) {
+    public function edit(UserRequest $request): void {
         $user = $request->except($request->password);
         $user['password'] = Hash::make($request->password);
 
         User::where('id', $request->id)->update($user);
     }
 
-    public function delete(Request $request) {
+    public function delete(Request $request): void {
         User::destroy($request->id);
     }
 }
