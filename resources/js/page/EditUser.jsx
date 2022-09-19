6f5d5bc3681245af27e1d45e9b229aebdae9c1c1
Andrey Nozdrenko <developer@devit.group>
2022-09-19 12:55:20 +0300

    fix

--- resources/js/page/EditUser.jsx
+++ resources/js/page/EditUser.jsx
@@ -1,13 +1,10 @@
 import Layout from "../components/Layout";
 import Edit from "../components/Edit";
-import { getUser } from "../helpers/http";
-import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import ErrorBoundary from "../errors/ErrorBoundary";
 import { useGetUserQuery } from "../store/queries/apiUser";
 
 export default function EditUser() {
-    let [user, setUser] = useState({});
     const {id} = useParams();
     const { data, error, isLoading } = useGetUserQuery(id);
 
